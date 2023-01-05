8a3b360ef44cf289365c90ae0f43e6077da0776d
ysknk <31568245+ysknk@users.noreply.github.com>
2023-01-05 19:08:56 +0900

    Create .imagemin-pluginsrc.cjs

---
+++ .imagemin-pluginsrc.cjs
@@ -0,0 +1,19 @@
+module.exports = [
+  require('imagemin-pngquant')({
+    quality: [0.5, 1.0]
+  }),
+  require('imagemin-mozjpeg')({
+    quality: 85,
+    progressive: true
+  }),
+  require('imagemin-gifsicle')(),
+  require('imagemin-svgo')({
+    plugins: [
+      { removeViewBox: false }
+    ]
+  }),
+  require('imagemin-webp')({
+    quality: 75,
+    lossless: true
+  })
+]
