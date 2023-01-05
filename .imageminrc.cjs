27e56a5884952eee18a0e4dbbb78afc8b7ad3c90
ysknk <31568245+ysknk@users.noreply.github.com>
2023-01-05 17:27:06 +0900

    Create .imageminrc.cjs

---
+++ .imageminrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  cwd: "./test/before/",
+  // src: "./test/before/**/[!_]*.{jpg,png,gif,svg}",
+  src: "**/[!_]*.{jpg,png,gif,svg}",
+  dest: "./test/after/",
+
+  webp: "\.(jpg|png)$",// NOTE: .gif not work
+
+  plugins: [
+    require('imagemin-pngquant')({
+      quality: [0.5, 1.0]
+    }),
+    require('imagemin-mozjpeg')({
+      quality: 85,
+      progressive: true
+    }),
+    require('imagemin-gifsicle')(),
+    require('imagemin-svgo')({
+      plugins: [
+        { removeViewBox: false }
+      ]
+    }),
+    require('imagemin-webp')({
+      quality: 75,
+      lossless: true
+    })
+  ]
+}
+
